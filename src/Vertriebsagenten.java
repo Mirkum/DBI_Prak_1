cc6a8e3e71f04f3684584b0fcab84921fbd6e8ff
MazzenYo <56998097+MazzenYo@users.noreply.github.com>
2021-11-09 12:40:37 +0100

    Änderung System.out.println Z.57

--- src/Vertriebsagenten.java
+++ src/Vertriebsagenten.java
@@ -54,7 +54,7 @@ public class Vertriebsagenten {
 
                     System.out.println();
                     System.out.println("AID|        ANAME|    DOLLARS");
-                    System.out.println("---|-------------|----------- du arsch");
+                    System.out.println("---|-------------|-----------");
 
                     while (rs.next())
                     {
